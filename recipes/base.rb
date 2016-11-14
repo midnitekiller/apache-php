67bcf928b12f122d821da74cb2af29744600606a
midnitekiller <liefjillcolegado@gmail.com>
2016-11-15 00:37:53 +0800

    apache config

--- recipes/base.rb
+++ recipes/base.rb
@@ -9,11 +9,21 @@ include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "apache2::mod_php5"
 include_recipe "php"
+
+
+web_app "my_site" do
+      server_name node['hostname']
+      server_aliases [node['fqdn'], "my-site.example.com"]
+      docroot "/srv/www/my_site"
+      cookbook 'apache2'
+end
+
+
 # include_recipe "php::mod_php5"
 
-default['apache']['default_site_name'] = 'phpapp'
-default['apache']['default_site_enabled'] = true
+# default['apache']['default_site_name'] = 'phpapp'
+# default['apache']['default_site_enabled'] = true
 
-apache_site "default" do
-  enable true
-end
+# apache_site "default" do
+#   enable true
+# end
