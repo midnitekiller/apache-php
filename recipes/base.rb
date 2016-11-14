60b46e171e47c1a2b0b8888e22d614e81c8798fc
midnitekiller <liefjillcolegado@gmail.com>
2016-11-15 01:48:27 +0800

    changing permissions

--- recipes/base.rb
+++ recipes/base.rb
@@ -9,10 +9,18 @@ include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "apache2::mod_php5"
 include_recipe "php"
+include_recipe "php::module_mysql"
 
-web_app "my_site" do
+web_app "aetdeploy" do
       server_name node['hostname']
-      server_aliases [node['fqdn'], "my_site.com"]
-      docroot "/srv/www/my_site"
+      server_aliases [node['fqdn'], "aetdeploy.com"]
+      docroot "/var/www/aetdeploy/"
       cookbook 'apache2'
+end
+
+directory '/var/www/aetdeploy/' do
+  owner 'www-data'
+  group 'www-data'
+  mode '0755'
+  action :create
 end
