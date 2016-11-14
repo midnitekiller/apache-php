4b25258dd4e48c372d7021ed359928d795779a9e
midnitekiller <liefjillcolegado@gmail.com>
2016-11-14 23:57:47 +0800

    config base recipe

--- recipes/base.rb
+++ recipes/base.rb
@@ -9,7 +9,7 @@ include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "apache2::mod_php5"
 include_recipe "php"
-include_recipe "php::mod_php5"
+# include_recipe "php::mod_php5"
 
 
 apache_site "default" do
