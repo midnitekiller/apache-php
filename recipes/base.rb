3b0a5f81b2b90b598ff47a225bac6c0fb65f9e6d
midnitekiller <liefjillcolegado@gmail.com>
2016-11-15 02:18:26 +0800

    edit recipe

--- recipes/base.rb
+++ recipes/base.rb
@@ -9,16 +9,16 @@ include_recipe "apache2"
 include_recipe "mysql::client"
 include_recipe "apache2::mod_php5"
 include_recipe "php"
-include_recipe "php::module_mysql"
+# include_recipe "php::module_mysql"
 
 web_app "aetdeploy" do
       server_name node['hostname']
       server_aliases [node['fqdn'], "aetdeploy.com"]
-      docroot "/var/www/aetdeploy/"
+      docroot "/var/www/aetdeploy"
       cookbook 'apache2'
 end
 
-directory '/var/www/aetdeploy/' do
+directory '/var/www/aetdeploy' do
   owner 'www-data'
   group 'www-data'
   mode '0755'
